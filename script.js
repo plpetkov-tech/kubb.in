afd93a5ff98351199e40e7225b46051e66c2ddf7
plpetkov-tech <plpetkov@tutanota.com>
2021-02-16 11:07:08 +0200

    merged collection of user and sub, and develop the frontend

--- script.js
+++ script.js
@@ -9,11 +9,5 @@ toggle.addEventListener('click', ()=>
     document.body.classList.toggle('show-nav')
 );
 
-// Show modal
-open.addEventListener('click', ()=> modal.classList.add('show-modal'));
 
-// Hide modal
-close.addEventListener('click', ()=> modal.classList.remove('show-modal'));
-
-// Hide modal on outside click
-window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false)
+open.addEventListener('click', ()=> window.location.href = './kubb.in-frontend/login/register.html');
