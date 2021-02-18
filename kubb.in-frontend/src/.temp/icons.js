dd8947f75186127d32d4c68a0334258b95e6b7fc
plpetkov-tech <plpetkov@tutanota.com>
2021-02-18 14:10:55 +0200

    migrate from vanilla js to vuejs

---
+++ kubb.in-frontend/src/.temp/icons.js
@@ -0,0 +1,41 @@
+export default {
+  "touchiconMimeType": "image/png",
+  "faviconMimeType": "image/png",
+  "precomposed": false,
+  "touchicons": [
+    {
+      "width": 76,
+      "src": "/assets/static/src/favicon.png?width=76&key=fc3c058"
+    },
+    {
+      "width": 152,
+      "src": "/assets/static/src/favicon.png?width=152&key=fc3c058"
+    },
+    {
+      "width": 120,
+      "src": "/assets/static/src/favicon.png?width=120&key=fc3c058"
+    },
+    {
+      "width": 167,
+      "src": "/assets/static/src/favicon.png?width=167&key=fc3c058"
+    },
+    {
+      "width": 180,
+      "src": "/assets/static/src/favicon.png?width=180&key=fc3c058"
+    }
+  ],
+  "favicons": [
+    {
+      "width": 16,
+      "src": "/assets/static/src/favicon.png?width=16&key=f434e92"
+    },
+    {
+      "width": 32,
+      "src": "/assets/static/src/favicon.png?width=32&key=f434e92"
+    },
+    {
+      "width": 96,
+      "src": "/assets/static/src/favicon.png?width=96&key=f434e92"
+    }
+  ]
+}
---
+++ kubb.in-frontend/src/pages/Login.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Nunito"
+    />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.0.6/css/all.css"
+    />
+    <link rel="stylesheet" href="login.css" />
+    <div id="login">
+      <div id="description">
+        <h1>Kubb.in</h1>
+        <p>
+          By logging in you agree to the ridiculously long terms that you didn't
+          bother to read. If you don't have an account register
+          <a href="/register">here</a>
+        </p>
+      </div>
+      <div id="form">
+        <form @submit.prevent="doLogin">
+          <label for="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            v-model="username"
+            placeholder="username"
+            autocomplete="off"
+          />
+
+          <label for="password">Password</label>&nbsp;
+          <i
+            class="fas"
+            :class="[passwordIcon]"
+            @click="hidePassword = !hidePassword"
+          ></i>
+          <input
+            :type="passwordType"
+            id="password"
+            v-model="password"
+            placeholder="**********"
+          />
+
+          <button type="submit">Log in</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Login Page",
+  },
+  data() {
+    return {
+      username: "",
+      password: "",
+      hidePassword: true,
+      jsonRequest: {},
+      user: localStorage.getItem("user"),
+    };
+  },
+  created() {
+    if (this.user != null) {
+      if (this.user.includes("ROLE_USER")) {
+        window.location.href = "/main";
+      }
+    }
+  },
+  computed: {
+    passwordType() {
+      return this.hidePassword ? "password" : "text";
+    },
+    passwordIcon() {
+      return this.hidePassword ? "fa-eye" : "fa-eye-slash";
+    },
+  },
+  methods: {
+    doLogin() {
+      sendLoginRequest(`${username.value}`, `${password.value}`);
+    },
+    checkIfLogged() {
+      console.log("jsontoken ", data.user);
+    },
+  },
+};
+function sendLoginRequest(u, p) {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  var raw = JSON.stringify({ username: u, password: p });
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch("https://kubb.in:8080/api/auth/signin", requestOptions)
+    .then((response) => response.json())
+    .then((result) =>
+      result.error
+        ? console.log("wrong password todo")
+        : redirectWithToken(result)
+    )
+    .catch((error) => console.log("error", error));
+}
+
+function redirectWithToken(token) {
+  if (token !== undefined) {
+    localStorage.setItem("user", JSON.stringify(token));
+    window.location.href = "/main";
+  }
+}
+</script>
